6a9d12b316ca94ce0a52b1fe8612f2db64a214d2
Yuliya Voronovich <Y-1306@mail.ru>
2017-03-02 14:20:05 +0300

    02/03/2017

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,11 @@
 var myName="Voronovich Yuliya";
 var myRole="Web developer";
-var myContact="8 (029) 762 40 18";
+var myContact={
+	mobile:"8 (029) 762 40 18",
+	email:"Y-1306@mail.ru",
+	github:"YuliyaVoronovich",
+	location:"Minsk"
+}
 var myPictureUrl="https://pp.userapi.com/c623829/v623829246/49459/bQNU9UX4rWI.jpg";
 var mySkills=['php','js','mathematics','html','css'];
 
@@ -9,8 +14,40 @@ var bio={
 	role:myRole,
 	contact:myContact,
 	pictureUrl:myPictureUrl,
-	welcomeMessage:"Hello! I am "+name,
+	welcomeMessage:"Hello! I am "+myName,
 	skills:mySkills
+}
+var education={
+	"shools":[
+		{
+			"name":"Cymnasium №2",
+			"city":"Polotsk",
+			"year":2007
+		},
+		{
+			"name":"BSUIR",
+			"city":"Minsk",
+			"year":2012
+		}
+	],
+	"onlineCurses":[
+		{
+			"title":"JavaScript Basics",
+			"school":"Udacity",
+			"year":2017
+		}
+	]
+}
+var work={
+	"jobs":[
+		{
+			"employer":"OOO Belan technologies",
+			"city":"Minsk",
+			"dates": Avg 2012-Future,
+			"position": IT Engineer
+		}
+		
+	]
 }
 
 var formattedName=HTMLheaderName.replace("%data%",bio.name);
